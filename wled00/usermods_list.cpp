32ceeb6d2957578dd7b492036b877e42a82e7846
Sascha Volkenandt <sascha@akv-soft.de>
2022-06-25 23:48:54 +0200

    implement analog clock as a usermod

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -128,6 +128,10 @@
 #include "../usermods/Si7021_MQTT_HA/usermod_si7021_mqtt_ha.h"
 #endif
 
+#ifdef USERMOD_ANALOG_CLOCK
+#include "../usermods/Analog_Clock/Analog_Clock.h"
+#endif
+
 void registerUsermods()
 {
 /*
@@ -242,5 +246,9 @@ void registerUsermods()
   
   #ifdef USERMOD_SI7021_MQTT_HA
   usermods.add(new Si7021_MQTT_HA());
+  #endif
+
+  #ifdef USERMOD_ANALOG_CLOCK
+  usermods.add(new AnalogClockUsermod());
   #endif
 }
